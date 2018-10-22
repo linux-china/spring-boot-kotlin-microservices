37460b1b61e1cc304b475a08da6f6595370a048d
linux_china <libing.chen@gmail.com>
2018-10-22 14:50:15 -0700

    polishing

--- microapp-1/src/main/java/org/mvnsearch/app1/PortalController.kt
+++ microapp-1/src/main/java/org/mvnsearch/app1/PortalController.kt
@@ -1,9 +1,12 @@
 package org.mvnsearch.app1
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 /**
  * portal controller
@@ -14,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController
 @RefreshScope
 class PortalController {
     @Value("\${developer:'jacky'}")
-    private val nick: String? = null
+    lateinit var nick: String
+    @Autowired
+    lateinit var discoveryClient: DiscoveryClient
 
     @GetMapping("/")
     fun index(): String {
-        return "hello" + nick!!
+        return "hello ${nick}"
     }
 
     @GetMapping("/welcome")
