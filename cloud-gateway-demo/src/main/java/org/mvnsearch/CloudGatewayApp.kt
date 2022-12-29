1e940f20566c8a388e60e024cf33bc91cebc11ca
Libing Chen <libing.chen@gmail.com>
2022-12-29 12:48:55 +0800

    chore: API adjusted

--- cloud-gateway-demo/src/main/java/org/mvnsearch/CloudGatewayApp.kt
+++ cloud-gateway-demo/src/main/java/org/mvnsearch/CloudGatewayApp.kt
@@ -3,24 +3,22 @@ package org.mvnsearch
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.filters
-import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
 open class CloudGatewayApp {
     @Bean
-    open fun additionalRouteLocator(builder: RouteLocatorBuilder) = builder.routes {
-        route(id = "test-kotlin") {
-            host("kotlin.abc.org")
-            path("/httpbin/**")
-            filters {
-                stripPrefix(1)
-            }
-            uri("http://httpbin.org")
+    open fun additionalRouteLocator(builder: RouteLocatorBuilder) = builder.routes()
+        .route { p ->
+            p.path("/httpbin/**")
+                .filters { f ->
+                    f.addRequestHeader("Hello", "World");
+                    f.stripPrefix(1)
+                }
+                .uri("http://httpbin.org:80")
         }
-    }
+        .build()
 }
 
 fun main(args: Array<String>) {
